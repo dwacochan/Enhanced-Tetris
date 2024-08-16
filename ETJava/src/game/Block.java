405c2b5b5b9f5b65a2cbd29bc90c4a47d9459fca
Anton Koulakov <anton.koulakov@griffithuni.edu.au>
2024-08-16 11:42:19 +1000

    Added game field and basic rendering + falling

---
+++ ETJava/src/game/Block.java
@@ -0,0 +1,19 @@
+package tetrominos;
+
+import java.awt.*;
+
+public class Block extends Rectangle {
+    public int x;
+    public int y;
+    public static final int SIZE = 20;
+    public Color color;
+
+    public Block(Color color){
+        this.color = color;
+    }
+
+    public void draw(Graphics2D g2d){
+        g2d.setColor(color);
+        g2d.fillRect(x, y, SIZE, SIZE);
+    }
+}
---
+++ ETJava/src/game/Tetromino.java
@@ -0,0 +1,72 @@
+package tetrominos;
+
+import game.Controls;
+
+import java.awt.*;
+
+public class Tetromino {
+    public Block blocks[] = new Block[4];
+    public Block tempBlocks[] = new Block[4];
+    private boolean falling = false;
+
+    public void create(Color color){
+        blocks[0] = new Block(color);
+        blocks[1] = new Block(color);
+        blocks[2] = new Block(color);
+        blocks[3] = new Block(color);
+        tempBlocks[0] = new Block(color);
+        tempBlocks[1] = new Block(color);
+        tempBlocks[2] = new Block(color);
+        tempBlocks[3] = new Block(color);
+    }
+
+    public void setPosition(int x, int y){
+
+    }
+
+    public void updatePosition(int rotation){
+
+    }
+
+    public void update(){
+        if (Controls.left){
+            blocks[0].x -= Block.SIZE;
+            blocks[1].x -= Block.SIZE;
+            blocks[2].x -= Block.SIZE;
+            blocks[3].x -= Block.SIZE;
+        }
+
+        if (Controls.right){
+            blocks[0].x += Block.SIZE;
+            blocks[1].x += Block.SIZE;
+            blocks[2].x += Block.SIZE;
+            blocks[3].x += Block.SIZE;
+        }
+
+        if (Controls.down){
+            blocks[0].y += Block.SIZE;
+            blocks[1].y += Block.SIZE;
+            blocks[2].y += Block.SIZE;
+            blocks[3].y += Block.SIZE;
+
+            Controls.down = false;
+        }
+
+        falling = true;
+        if (falling) {
+            blocks[0].y += 1;
+            blocks[1].y += 1;
+            blocks[2].y += 1;
+            blocks[3].y += 1;
+            falling = false;
+        }
+    }
+
+    public void draw(Graphics2D g2d){
+        g2d.setColor(blocks[0].color);
+        g2d.fillRect(blocks[0].x, blocks[0].y, Block.SIZE, Block.SIZE);
+        g2d.fillRect(blocks[1].x, blocks[1].y, Block.SIZE, Block.SIZE);
+        g2d.fillRect(blocks[2].x, blocks[2].y, Block.SIZE, Block.SIZE);
+        g2d.fillRect(blocks[3].x, blocks[3].y, Block.SIZE, Block.SIZE);
+    }
+}
